072694636fc4ab056d15ccd50abd6041e1ea5ebe
andrianov_ng <andrianov93@inbox.ru>
2021-12-04 18:11:13 +0300

    hw

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,55 @@
 public class Main {
+    public static void main(String[] args) {
+        Object mon = new Object();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (mon) {
+                    for (int i = 0; i < 5; i++) {
+                        System.out.print("A");
+                        try {
+                            mon.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (mon) {
+                    for (int i = 0; i < 5; i++) {
+                        System.out.print("B");
+                        try {
+                            mon.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        mon.notify();
+                    }
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (mon){
+                    for (int i = 0; i < 5; i++) {
+                        mon.notifyAll();
+                        System.out.print("C");
+                        try {
+                            mon.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }).start();
+    }
 }
